1fc9e7da68fb378a0b80b740df7c853de08ec06f
Stefano Braconi <sugeppe@gmail.com>
2024-11-14 11:42:10 +0100

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
 
-function validateForm() {
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirm_password').value;
-    if (password !== confirmPassword) {
-        alert('Le psasword non corrispondono');
-        return false;
-    }
-    return true;
-}
